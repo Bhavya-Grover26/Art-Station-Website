6b524b12f901bd0dbbd3db8b6e224cde68b2ddfe
Bhavya Grover <bhavya2526@gmail.com>
2023-06-08 15:58:02 +0530

    second commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,55 @@
-import React, { useState } from 'react';
+import React,{useContext,useRef,useEffect,useState} from 'react'
 import { useNavigate, Link } from 'react-router-dom';
 import { PrimaryNav, MenuLink, Menu, Hamburger, Logo, PostButton, SearchBar } from './NavElement';
 import {AiFillMessage} from 'react-icons/ai';
 import { FaBell } from 'react-icons/fa';
 import { BsFillPersonFill } from 'react-icons/bs';
 import logoImg from './Brush Strokes.png';
+import {UserContext} from '../../App'
+import M from 'materialize-css'
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
+  const {state,dispatch} = useContext(UserContext)
+  const renderList = ()=>{
+    if(state){
+      return[
+        <SearchBar
+    type="text"
+    placeholder="Search..."
+    value={searchTerm}
+    onChange={handleInputChange}
+  />,
+  <Link to="/createpost">
+          <PostButton>Post</PostButton>
+        </Link>,
+  <Menu>
+        
+        <MenuLink to="/illustration" activeStyle>
+        <BsFillPersonFill />
+      </MenuLink>
+      <MenuLink to="/notifications" activeStyle>
+        <FaBell />
+      </MenuLink>
+      <MenuLink to="/message" activeStyle>
+        <AiFillMessage />
+    </MenuLink>
+    </Menu>
+    
+      ]
+    }
+    else{
+      return[
+        <Menu>
+        <MenuLink to="/login" activeStyle>
+            Login
+          </MenuLink>
+          </Menu>
+      ]
+
+    }
+  }
 
   const handleClickLogo = () => {
     navigate('/');
@@ -29,37 +70,12 @@ const Navbar = () => {
   return (
     <>
       <PrimaryNav>
-        <Link to="/">
+        <Link to={state?"/":"/login"}>
           <Logo onClick={handleClickLogo} src={logoImg} alt="Logo" />
         </Link>
 
-        <SearchBar
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={handleInputChange}
-          
-        />
-
-        <Link to="/createpost">
-          <PostButton>Post</PostButton>
-        </Link>
-
-        <Menu>
-        <MenuLink to="/login" activeStyle>
-            Login
-          </MenuLink>
-          <MenuLink to="/illustration" activeStyle>
-            <BsFillPersonFill />
-          </MenuLink>
-          <MenuLink to="/notifications" activeStyle>
-            <FaBell />
-          </MenuLink>
-          <MenuLink to="/message" activeStyle>
-            <AiFillMessage />
-        </MenuLink>
-        
-        </Menu>
+        {renderList()}
+       
       </PrimaryNav>
     </>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,95 +1,48 @@
-import React, { useState } from 'react';
+import React,{useState,useEffect,useContext} from 'react'
 import './Home.css';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 function Home() {
-  const [likedImages, setLikedImages] = useState([]);
-  const [likesCount, setLikesCount] = useState({});
-  const [enlargedImages, setEnlargedImages] = useState({});
-
-  const handleLikeClick = (imageId) => {
-    setLikedImages((prevLikedImages) => {
-      if (prevLikedImages.includes(imageId)) {
-        return prevLikedImages.filter((id) => id !== imageId);
-      } else {
-        return [...prevLikedImages, imageId];
-      }
-    });
-
-    setLikesCount((prevLikesCount) => {
-      if (prevLikesCount.hasOwnProperty(imageId)) {
-        if (likedImages.includes(imageId)) {
-          return { ...prevLikesCount, [imageId]: prevLikesCount[imageId] - 1 };
-        } else {
-          return { ...prevLikesCount, [imageId]: prevLikesCount[imageId] + 1 };
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    fetch('/allpost',{
+        headers:{
+            "Authorization":"Bearer "+localStorage.getItem("jwt")
         }
-      } else {
-        return { ...prevLikesCount, [imageId]: 1 };
-      }
-    });
-  };
-  
-
-  const handleImageClick = (imageId) => {
-    setEnlargedImages((prevEnlargedImages) => {
-      return { ...prevEnlargedImages, [imageId]: !prevEnlargedImages[imageId] };
-    });
-  };
+    }).then(res=>res.json())
+    .then(result=>{
+        console.log(result)
+        setData(Object.values(result));
+    })
+ },[])
 
   return (
-    <div className="container">
+    <div className="home">
+      {
+        data && data.map(item=>{
+          return(
+            <div className="container" key={item._id}>
       <div className="image-row">
-        <div className={`image-card ${enlargedImages['image1'] ? 'enlarged' : ''}`}>
+        <div className="image-card">
           <img
-            src="https://i.ibb.co/tHbw3wK/7117bad2-a7cd-464c-88f4-823981a7ca00.jpg"
+            src={item.image}
             alt="Image 1"
             className="image"
-            onClick={() => handleImageClick('image1')}
           />
-          <div className="image-title">Image 1</div>
+          <div className="image-title">{item.caption}</div>
           <div className="profile-info">
-            <img
-              src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces"
-              alt="Profile"
-              className="profile-picture"
-            />
-            <div className="username">User1</div>
+            <div className="username">{item.postedBy && item.postedBy.username}</div>
           </div>
-          <button
-            className={`heart-emoji ${likedImages.includes('image1') ? 'active' : ''}`}
-            onClick={() => handleLikeClick('image1')}
-          >
-            {likedImages.includes('image1') ? <AiFillHeart /> : <AiOutlineHeart />}
-          </button>
-          <div className="likes-count">{likesCount['image1'] || 0} likes</div>
+          
         </div>
-        <div className={`image-card ${enlargedImages['image2'] ? 'enlarged' : ''}`}>
-          <img
-            src="https://i.ibb.co/tcTPRkL/Whats-App-Image-2022-04-03-at-12-10-49-AM-1.jpg"
-            alt="Image 2"
-            className="image"
-            onClick={() => handleImageClick('image2')}
-          />
-          <div className="image-title">Image 2</div>
-          <div className="profile-info">
-            <img
-              src="https://i.ibb.co/tHbw3wK/7117bad2-a7cd-464c-88f4-823981a7ca00.jpg"
-              alt="Profile"
-              className="profile-picture"
-            />
-            <div className="username">User2</div>
-            
-          </div>
-          <button
-            className={`heart-emoji ${likedImages.includes('image2') ? 'active' : ''}`}
-            onClick={() => handleLikeClick('image2')}
-          >
-            {likedImages.includes('image2') ? <AiFillHeart /> : <AiOutlineHeart />}
-          </button>
-          <div className="likes-count">{likesCount['image2'] || 0} likes</div>
-        </div>
-        {/* Add more image cards */}
       </div>
+    </div>
+          )
+        })
+      }
+    
+                   
+       
     </div>
   );
 }
